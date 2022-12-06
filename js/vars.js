e322af3cd2e76dcfc29ff1f89afa66d5f09d6743
James S <smith7@uleth.ca>
2022-12-06 14:07:57 -0700

    implement first half of strings

--- js/vars.js
+++ js/vars.js
@@ -1,66 +1,66 @@
 var a1 = "Adrift: Even if you feel that you are going adrift in life, that is completely fine. Nobody has things figured out 100% of the time so just take things at your own pace."
 var a2 = "Abolish: If there are any issues or toxicity in your life, I hope that you are able to abolish it for your own sake. You’ve got this!"
-var a3 = "aaaa"
+var a3 = "Adjust: There are times in our lives where we need to adjust. Sometimes though, you just can’t and that is completely okay. It does not make you any less of a person."
 var aList = [a1, a2, a3]
 
-var b1 = "bbbb"
-var b2 = "baaa"
-var b3 = "bkskere"
+var b1 = "Breathe: When you're feeling overwhelmed, remember to breathe and relax. You have the strength and resilience to overcome any challenge that comes your way."
+var b2 = "Brighten: Your smile can brighten someone's day, so don't be afraid to share it with the world. You never know how much a simple act of kindness can make a difference in someone's life."
+var b3 = "Befriend: Don't be afraid to befriend someone new and make new connections. The more people you know, the more support and guidance you'll have to help you navigate through life's challenges."
 var bList = [b1, b2, b3]
 
-var c1 = ""
-var c2 = ""
-var c3 = ""
+var c1 = "Commit: If you're going to do something, give it your all and commit to it fully. Whether it's a new project or a personal goal, dedication and perseverance are key to achieving success."
+var c2 = "Cherish: Take the time to cherish the little things in life. It's often the small moments and experiences that bring us the most joy and fulfillment."
+var c3 = "Champion: Be your own champion and don't let anyone else bring you down. You are strong and capable, and you have the power to overcome any obstacle and achieve your dreams"
 var cList = [c1, c2, c3]
 
-var d1 = ""
-var d2 = ""
-var d3 = ""
+var d1 = "Discover: Take the time to discover who you are and what you want in life. Life is a journey of self-discovery, so embrace it and learn as much as you can about yourself and the world around you."
+var d2 = "Dream: Dream big and don't let anyone tell you that your goals are impossible to achieve. With hard work, determination, and a positive attitude, you can make your wildest dreams a reality."
+var d3 = "Dazzle: Dazzle the world with your unique talents and abilities. You have something special to offer, so don't be afraid to share it with others and shine bright like the star that you are."
 var dList = [d1, d2, d3]
 
-var e1 = ""
-var e2 = ""
-var e3 = ""
+var e1 = "Enjoy: Life is too short to not enjoy every moment you can. So don't be afraid to stop and smell the roses!"
+var e2 = "Empower: You have the strength and power within you to accomplish whatever you set your mind to. Empower yourself and go after your dreams."
+var e3 = "Encourage: No matter how tough things may seem, don't forget that you have the ability to encourage others and make a positive difference in their lives. You are capable of great things!"
 var eList = [e1, e2, e3]
 
-var f1 = ""
-var f2 = ""
-var f3 = ""
+var f1 = "Fervor: Don't lose your fervor for the things you are passionate about. It is your enthusiasm and drive that will help you achieve your goals."
+var f2 = "Flourish: You have the potential to flourish and grow in ways you never thought possible. Believe in yourself and you will go far."
+var f3 = "Fulfill: Life is full of opportunities to fulfill your dreams and goals. Don't let them pass you by – seize the day and make your dreams a reality!"
 var fList = [f1, f2, f3]
 
-var g1 = ""
-var g2 = ""
-var g3 = ""
+var g1 = "Gratitude: Practicing gratitude can help you appreciate the good things in your life and find joy in the present moment. Be thankful for what you have, and you will be rewarded with happiness and contentment."
+var g2 = "Generous: You are a generous person. Even in times where you may feel selfish, just know that many are blessed to even be in your life. So even if you feel that you aren’t giving anything, just by being around, you are giving lots."
+var g3 = "Grace: Showing grace and kindness to others, even when they may not deserve it, can be a powerful way to make the world a better place. You are the light in someone's day and spread love and positivity wherever you go."
 var gList = [g1, g2, g3]
 
-var h1 = ""
-var h2 = ""
-var h3 = ""
+var h1 = "Heal: You have the power to heal yourself, both physically and emotionally. Don't be afraid to seek help and support when you need it, and trust in your own ability to recover and grow."
+var h2 = "Honor: You deserve to be honored and valued for the unique person you are. Don't let anyone tell you otherwise – believe in yourself and your worth."
+var h3 = "Hope: No matter how difficult things may seem, never lose hope. You are stronger than you realize, and you have the power to overcome any challenge that comes your way. Keep your head held high and never give up."
 var hList = [h1, h2, h3]
 
-var i1 = ""
-var i2 = ""
-var i3 = ""
+var i1 = "Inspire: You are capable of inspiring yourself and others with your inner strength and positive attitude. Believe in yourself and your abilities, and let that confidence shine through."
+var i2 = "Innovate: You have the power to innovate and find unique solutions to your problems. Don't be afraid to think outside the box and trust in your own creativity and ingenuity."
+var i3 = "Improve: Every day is a chance to improve yourself and your life. Set goals, work hard, and never stop striving to be the best you can be. Remember that you are capable of great things, and have faith in your own abilities."
 var iList = [i1, i2, i3]
 
-var j1 = ""
-var j2 = ""
-var j3 = ""
+var j1 = "Journey: Life is a journey, full of ups and downs, twists and turns. Embrace the adventure and trust in your own ability to navigate whatever comes your way."
+var j2 = "Joy: You have the power to create joy in your own life, and to spread it to those around you. Don't let negativity or doubt get in the way – focus on the good and let your happiness shine through."
+var j3 = "Juggle: You are capable of juggling multiple tasks, responsibilities, and challenges at once. Trust in your own strength and resilience, and don't be afraid to ask for help when you need it. You've got this!"
 var jList = [j1, j2, j3]
 
-var k1 = ""
-var k2 = ""
-var k3 = ""
+var k1 = "Knowledge: Your thirst for knowledge and learning makes you strong and capable. Never stop seeking new information and experiences – they will help you grow and improve."
+var k2 = "Keep: Keep going, even when things get tough. You are stronger than you realize, and you have the power to overcome any obstacle that comes your way. Keep pushing forward, and never give up on your dreams."
+var k3 = "Kudos: Don’t forget to give yourself a little kudos for all of the hard work and effort that you put in. You deserve to be proud of yourself and your accomplishments."
 var kList = [k1, k2, k3]
 
-var l1 = ""
-var l2 = ""
-var l3 = ""
+var l1 = "Lively: Even on your worst days, try to stay lively and positive. A little bit of energy and enthusiasm can help you stay motivated and focused on your goals."
+var l2 = "Loyalty: Be loyal to yourself and your values. Don’t be afraid to stand up for what you believe in, and don’t compromise on your principles."
+var l3 = "Love: Above all else, remember to love and accept yourself for who you are. Love is the foundation for a healthy and fulfilling life, so make sure to nurture it in yourself and others."
 var lList = [l1, l2, l3]
 
-var m1 = ""
-var m2 = ""
-var m3 = ""
+var m1 = "Magic: Believe in yourself and the magic of possibility. You are capable of achieving great things, so don’t let anyone else tell you otherwise."
+var m2 = "Mentor: I hope there is someone in your life that can mentor and guide you to becoming the best version of yourself."
+var m3 = "Meaning: You deserve nothing less than the very best and I hope you are able to come up with what the meaning of your life is."
 var mList = [m1, m2, m3]
 
 var n1 = ""
